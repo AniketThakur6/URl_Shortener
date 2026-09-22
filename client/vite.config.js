ead942b9d5ea5f5a21040b98db94dc9e107504af
AniketThakur6 <aniketthakur1928@gmail.com>
2026-09-22 13:22:34 +0530

    Update API base URL to use environment variable and adjust Vite proxy settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: env.VITE_API_URL,
-//         changeOrigin: true,
-//         secure: true,
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -28,9 +10,9 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         "/api": {
-          target: env.VITE_API_URL,
+          target: env.VITE_API_URL || "http://localhost:3000",
           changeOrigin: true,
-          secure: true,
+          secure: false,
         },
       },
     },
